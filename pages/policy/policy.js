b4d88dba77aabbf86b39500704448db39f091dfd
rcy <21010078@mail.ecust.edu.cn>
2025-02-14 10:26:33 +0800

    policy

---
+++ pages/policy/policy.js
@@ -0,0 +1,96 @@
+// pages/policy/policy.js
+import utils from '../../utils/util.js';
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    userinfo:{},
+    className:"",
+    policyDetailData:"",
+  },
+
+  getPolicyDetail: function(policy_name){
+    let url = "/policy/" + policy_name
+    let method = "POST"
+    let data = {}
+    utils.request(url, method, data)
+    .then(res => {
+        console.log(res)
+        this.setData({
+          policyDetailData: res.data
+      });
+    })
+    .catch(err => {
+      console.log(err)
+    });
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+    let userinfo = wx.getStorageSync("userinfo")
+    let className = wx.getStorageSync("curClass")
+    this.setData({
+      className: className,
+      userinfo: userinfo,
+    })
+    let { policy_name } = options
+    if(policy_name){
+      this.getPolicyDetail(policy_name)
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+    this.onLoad(this.options)
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
