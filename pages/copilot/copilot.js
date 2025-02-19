1244f3ba54f0dc89b9dff9308b40dbdfe395a26b
rcy <21010078@mail.ecust.edu.cn>
2025-02-20 00:00:10 +0800

    cot agent front 1

--- pages/copilot/copilot.js
+++ pages/copilot/copilot.js
@@ -1,17 +1,95 @@
 // pages/copilot/copilot.js
+import utils from '../../utils/util.js';
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    role:"",
+    userinfo:{},
+    className:"",
+    question: "",
+    answer: "",
+    questionHistory:"",
+    chatHistory: [],
+  },
 
+  bindInput: function(e) {
+    this.setData({
+      question: e.detail.value
+    });
+  },
+
+  askAgent: function(){
+    let newQuestion =  this.data.questionHistory + this.data.question;
+    if (newQuestion === "") {
+      wx.showToast({
+        title: '请输入问题',
+        icon: 'none'
+      });
+      return;
+    }
+    wx.showToast({
+      title: '问题提交成功',
+      icon: 'none'
+    });
+
+    let url = "/ai-agent/"
+    let method = "POST"
+    let data = {question:newQuestion}
+    utils.request(url, method, data)
+    .then(res => {
+        console.log(res)
+        console.log(res.data.ans)
+        this.setData({
+          answer: res.data.ans,
+          chatHistory: [...this.data.chatHistory, { question: this.data.question, answer: res.data.ans }],
+          questionHistory: newQuestion
+        });
+        this.setData({
+          question: ""
+        });
+    })
+    .catch(err => {
+      console.log(err)
+      wx.showToast({
+        title: '请求失败',
+        icon: 'none'
+      });
+    });
+  
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
+    let userinfo = wx.getStorageSync("userinfo")
+    let className = wx.getStorageSync("curClass")
+    this.setData({
+      className: className,
+      userinfo: userinfo
+    })
+    
+    if(userinfo.teacher_info){
+      this.setData({
+        role: "teacher"
+      });
+    }
+    else if(userinfo.stu_info){
+      this.setData({
+        role: "stu"
+      });
+    }
+    else{
+      this.setData({
+        role: ""
+      });
+    }
+
+    // this.askAgent()
 
   },
 
@@ -47,7 +125,7 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh() {
-
+    this.onLoad(this.options)
   },
 
   /**
