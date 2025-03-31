0d71f0e5825489599b334016b6cc442e780212ad
rcy <21010078@mail.ecust.edu.cn>
2025-03-31 16:32:50 +0800

    announcement

--- pages/announcement-list/announcement-list.js
+++ pages/announcement-list/announcement-list.js
@@ -1,18 +1,46 @@
 // pages/announcement-list/announcement-list.js
+import utils from '../../utils/util.js';
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    userinfo:{},
+    className:"",
+    announcement_list:""
+  },
 
+
+  getAnnouncementList: function(){
+    let url = "/announcement-list/"+this.data.className
+    let method = "POST"
+    let data = {}
+    utils.request(url, method, data)
+    .then(res => {
+        console.log(res)
+        this.setData({
+          announcement_list: res.data.announcement_list
+      });
+    })
+    .catch(err => {
+      console.log(err)
+    });
+  
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    let userinfo = wx.getStorageSync("userinfo")
+    let className = wx.getStorageSync("curClass")
+    this.setData({
+      className: className,
+      userinfo: userinfo,
+    })
+    this.getAnnouncementList()
   },
 
   /**
@@ -47,7 +75,7 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh() {
-
+    this.onLoad(this.options)
   },
 
   /**
