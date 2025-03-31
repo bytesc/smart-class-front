79800fef4f32e0a38b005d6f71982d877c74f48d
rcy <21010078@mail.ecust.edu.cn>
2025-03-31 20:40:59 +0800

    message

--- pages/message/message.js
+++ pages/message/message.js
@@ -1,18 +1,63 @@
 // pages/message/message.js
+import utils from '../../utils/util.js';
+
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    userinfo:{},
+    className:"",
+    message_list:"",
+    openedMessages: []
+  },
 
+  getMessageList: function(){
+    if (this.data.userinfo==""){
+      wx.redirectTo({
+        url: '/pages/login/login',
+      });
+    }
+    let url = "/message-list/"+this.data.userinfo.uid
+    let method = "POST"
+    let data = {}
+    utils.request(url, method, data)
+    .then(res => {
+        console.log(res)
+        const openedMessages = new Array(res.data.message_list.length).fill(false)
+        this.setData({
+          openedMessages: openedMessages,
+          message_list: res.data.message_list
+      });
+    })
+    .catch(err => {
+      console.log(err)
+    });
+  
+  },
+
+  toggleMessage: function(e) {
+    const index = e.currentTarget.dataset.index;
+    const newOpenedMessages = [...this.data.openedMessages];
+    newOpenedMessages[index] = !newOpenedMessages[index];
+    this.setData({
+      openedMessages: newOpenedMessages
+    });
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    let userinfo = wx.getStorageSync("userinfo")
+    let className = wx.getStorageSync("curClass")
+    this.setData({
+      className: className,
+      userinfo: userinfo,
+    })
+    this.getMessageList()
   },
 
   /**
@@ -47,7 +92,7 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh() {
-
+    this.onLoad(this.options)
   },
 
   /**
