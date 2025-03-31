b079f274e10ad9ac5b2ef8265b36c7541f0167ad
rcy <21010078@mail.ecust.edu.cn>
2025-03-31 23:28:17 +0800

    send an

--- pages/send-announcement/send-announcement.js
+++ pages/send-announcement/send-announcement.js
@@ -1,18 +1,59 @@
 // pages/send-announcement/send-announcement.js
+import utils from '../../utils/util.js';
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    userinfo:{},
+    className:"",
+  },
 
+  onSendForm: function(e) {
+    const method = 'POST';
+    const { announcement_name, content } = e.detail.value; 
+    let class_name = this.data.className
+    const data = { announcement_name, class_name, content };
+    const url = "/send-announcement/"
+
+    wx.showLoading({
+      title: '发送中',
+      mask: true
+    });
+
+    utils.request(url, method, data)
+      .then(res => {
+        wx.hideLoading();
+        console.log(res)
+        if(res.code===200){
+          wx.navigateBack()
+        }else{
+
+        }
+
+      })
+      .catch(err => {
+        wx.hideLoading();
+      });
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    let userinfo = wx.getStorageSync("userinfo")
+    let className = wx.getStorageSync("curClass")
+    this.setData({
+      className: className,
+      userinfo: userinfo,
+    })
+    if (this.data.userinfo==""){
+      wx.redirectTo({
+        url: '/pages/login/login',
+      });
+    }
   },
 
   /**
